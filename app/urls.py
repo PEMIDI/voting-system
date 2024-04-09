875db49aeaa631420542a2b0807f919ce15ce158
pemidi <rashidi.peyman@gmail.com>
2024-04-09 09:59:39 +0330

    fix urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, VoteView
 
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='article-list'),
+    path('vote/', VoteView.as_view(), name='vote')
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,7 @@
-
 from django.contrib import admin
 from django.urls import path, include
-
-from app.views import VoteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-    path('vote/', VoteView.as_view(), name='vote')
 ]
