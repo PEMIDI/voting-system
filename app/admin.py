f181ee70afa363f280e05c9c0e5692ec15175cbc
pemidi <rashidi.peyman@gmail.com>
2024-04-09 09:51:45 +0330

    add admin models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Article, Vote
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    search_fields = ('title',)
+    ordering = ('-created_at',)
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'article', 'score', 'created_at')
+    search_fields = ('user__username', 'article__title')
+    ordering = ('-created_at',)
